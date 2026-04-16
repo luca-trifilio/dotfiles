995075ec1463de0a6786e461b2c5481dfbad0d35
Luca Trifilio <luca.trifilio@gmail.com>
2026-04-16 23:21:43 +0200

    nvim: cleanup plugin configs

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -21,22 +21,6 @@ return {
         border_virtual = true,
         above = "▄",
         below = "▀",
-        foregrounds = {
-          "RenderMarkdownH1",
-          "RenderMarkdownH2",
-          "RenderMarkdownH3",
-          "RenderMarkdownH4",
-          "RenderMarkdownH5",
-          "RenderMarkdownH6",
-        },
-        backgrounds = {
-          "RenderMarkdownH1Bg",
-          "RenderMarkdownH2Bg",
-          "RenderMarkdownH3Bg",
-          "RenderMarkdownH4Bg",
-          "RenderMarkdownH5Bg",
-          "RenderMarkdownH6Bg",
-        },
       },
       code = {
         border = "thin",
