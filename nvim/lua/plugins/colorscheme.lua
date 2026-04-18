1615c40045fb3a95c3f6afd6f4ec1e756831f7b1
Luca Trifilio <luca.trifilio@gmail.com>
2026-04-18 15:51:28 +0200

    Catppuccin: nvim, starship tweaks

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-  { "luca-trifilio/dracula.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
