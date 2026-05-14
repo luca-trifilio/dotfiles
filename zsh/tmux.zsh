06d81b35d762336e2d97e982605d624f1ecc45fa
Luca Trifilio <lucatrifilio@MacBook-Pro-M4-Max-di-Luca.local>
2026-05-14 20:16:46 +0200

    tmux: fix default session name

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,7 +1,7 @@
 unalias tmux 2>/dev/null
 tmux() {
   if [[ $# -eq 0 ]]; then
-    command tmux new-session -A -s scratch
+    command tmux new-session -A -s home
   else
     command tmux "$@"
   fi
