f15ce1fb7d394ef584692c23debc0be8d29db186
Luca Trifilio <luca.trifilio@gmail.com>
2026-05-18 18:31:19 +0200

    zsh: k8s additions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,8 @@ alias python=python3
 alias pip=pip3
 alias cdd='cd ~'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias kns='kubens'
+alias kx='kubectx'
 
 function ts() { ~/.config/tmux/session-picker.sh }
 
